357f30aa78ebe9f5149729a93716bb01250ae943
Draszon <peti0202@windowslive.com>
2024-10-02 22:39:32 +0200

    Darts haladás

--- src/components/Darts.vue
+++ src/components/Darts.vue
@@ -3,38 +3,39 @@
   <h2 class="board-title">scoreboard</h2>
   <div class="scores-wrapper">
     <div class="names">
-      <h3 class="player-name">{{ players.firstPlayer.name }}</h3>
-      <h3 class="player-name">{{ players.secondPlayer.name }}</h3>
+      <h3 :class="{ 'current-player': firstPlayerRound }" class="player-name">{{ players.firstPlayer.name }}</h3>
+      <h3 :class="{ 'current-player': !firstPlayerRound }" class="player-name">{{ players.secondPlayer.name }}</h3>
     </div>
     <div class="score">
       <ul class="player-score">
-        <li></li>
+        <li v-for="score in players.firstPlayer.scoreList"> {{ score }}</li>
       </ul>
 
       <ul class="player-score">
-        <li></li>
+        <li v-for="score in players.secondPlayer.scoreList">{{ score }}</li>
       </ul>
     </div>
   </div>
   <div class="sum-wrapper">
-    <p></p>
-    <p></p>
+    <p>{{ players.firstPlayer.score }}</p>
+    <p>{{ players.secondPlayer.score }}</p>
   </div>
 </div>
 
-<div class="input-wrapper">
+<div v-if="!started" class="input-wrapper">
   <input v-model="players.firstPlayer.name" type="text" placeholder="Első játékos neve">
   <input v-model="players.secondPlayer.name" type="text" placeholder="Második játékos neve">
   <input v-model="gameType" type="text" placeholder="301 vagy 501">
-  <input style="cursor: pointer;" type="button" value="Kezdés">
+  <p v-if="error" style="color: red;">{{ error }}</p>
+  <input @click="startValidation()" style="cursor: pointer;" type="button" value="Kezdés">
 </div>
 
-<!--<div class="input-wrapper">
-  <input type="text" placeholder="Első dobás">
-  <input type="text" placeholder="Második dobás">
-  <input type="text" placeholder="Harmadik dobás">
-  <input style="cursor: pointer;" type="button" value="Rögzít">
-</div>-->
+<div v-else class="input-wrapper">
+  <input v-model="firstScore" type="text" placeholder="Első dobás">
+  <input v-model="secondScore" type="text" placeholder="Második dobás">
+  <input v-model="thirdScore" type="text" placeholder="Harmadik dobás">
+  <input @click="game()" style="cursor: pointer;" type="button" value="Rögzít">
+</div>
 </template>
 
 <script>
@@ -42,32 +43,95 @@ export default {
   data() {
     return {
       gameType: null,
+      started: false,
+      error: '',
+      firstScore: null,
+      secondScore: null,
+      thirdScore: null,
+      firstPlayerRound: true,
       players: {
         firstPlayer: {
           name: '',
-          score: 0
+          score: 0,
+          scoreList: []
         },
         secondPlayer: {
           name: '',
-          score: 0
+          score: 0,
+          scoreList: []
         }
       },
     }
   },
   methods: {
+    game() {
+      if (this.firstPlayerRound) {
+        const score = this.playerScore(
+          this.firstScore, this.secondScore, this.thirdScore
+        );
+        
+        this.updateScore(score, this.players.firstPlayer.score);
+        
+
+
+
+        this.players.firstPlayer.score -= score;
+        this.players.firstPlayer.scoreList.push(score);
+        this.scoreFieldReset();
+        this.firstPlayerRound = !this.firstPlayerRound;
+      } else {
+        const score = this.playerScore(
+          this.firstScore, this.secondScore, this.thirdScore
+        );
+        this.players.secondPlayer.score -= score;
+        this.players.secondPlayer.scoreList.push(score);
+        this.scoreFieldReset();
+        this.firstPlayerRound = !this.firstPlayerRound;
+      }
+    },
     //játék kezdésekor ellenőrzi megadott adatokat, hogy
     //megfelelnek-e a szabályoknak
-    game() {
-
+    startValidation() {
+      if (this.gameType != 301 && this.gameType != 501) {
+        this.error = 'A játék típusa csak 301 vagy 501 lehet!';
+      } else if (this.players.firstPlayer.name === '' && this.players.secondPlayer.name == '') {
+        this.error = 'Üres a játékosnév';
+      } else {
+        this.error = '';
+        this.started = !this.started;
+        this.players.firstPlayer.score = this.gameType;
+        this.players.secondPlayer.score = this.gameType;
+      }
     },
-    startFieldValidation() {
-
+    parseScore(score) {
+      let multiplier = 1;
+      if (score[0] === 'd') {
+        multiplier = 2;
+      } else if (score[0] === 't') {
+        multiplier = 3;
+      } else {
+        return parseInt(score);
+      }
+      return parseInt(score.slice(1)) * multiplier;
+    },
+    playerScore(first, second, third) {
+      return this.parseScore(first) + this.parseScore(second) + this.parseScore(third);
+    },
+    scoreFieldReset() {
+      this.firstScore = null;
+      this.secondScore = null;
+      this.thirdScore = null;
+    },
+    updateScore(current, pScore) {
+      
     }
   }
 }
 </script>
 
 <style scoped>
+.current-player { color: rgb(14, 218, 82); }
+
 .scoreboard {
   padding: 0.625rem;
   height: 32.5rem;
