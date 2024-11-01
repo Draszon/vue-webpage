56eb7c9eef0116cf802084c2cebf665c9894ee07
Draszon <peti0202@windowslive.com>
2024-11-01 19:16:46 +0100

    transitions

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -7,20 +7,29 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="container">
       <h2 class="section-title">JavaScript projektek</h2>
       <div class="row js">
+
         <div class="js-projects-wrapper">
-          <img class="arrow arrow-left" @click="gameMinus" src="/arrow-left.svg" alt="balra nyíl">
-          <div class="actual-jsproject" v-if="games">
-            <RouterLink :to="games[gameCounter].path" class="project-link">
-              <img  class="project-index" :src="games[gameCounter].img" alt="black jack indexkép">
-            </RouterLink>
-            <RouterLink :to="games[gameCounter].path" class="project-link">
-              <h3 class="project-title">{{ games[gameCounter].name }}</h3>
-            </RouterLink>
-            <p class="project-description">{{ games[gameCounter].description }}</p>
-          </div>
-          <div v-else><p>Nincsenek játékok!</p></div>
-          <img class="arrow arrow-right" @click="gamePlus" src="/arrow-right.svg" alt="jobbra nyíl">
+          <img class="arrow arrow-left" @click="gameMinus()" src="/arrow-left.svg" alt="balra nyíl">
+
+          <Transition name="games-slide">
+            <div class="actual-jsproject" v-if="games && exists">
+              <RouterLink :to="games[gameCounter].path" class="project-link">
+                <img  class="project-index" :src="games[gameCounter].img" alt="black jack indexkép">
+              </RouterLink>
+
+              <RouterLink :to="games[gameCounter].path" class="project-link">
+                <h3 class="project-title">{{ games[gameCounter].name }}</h3>
+              </RouterLink>
+              <p class="project-description">{{ games[gameCounter].description }}</p>
+            </div>
+            <!--<div v-else>
+              <p>Nincsenek játékok!</p>
+            </div>-->
+          </Transition>
+
+          <img class="arrow arrow-right" @click="gamePlus()" src="/arrow-right.svg" alt="jobbra nyíl">
         </div>
+
       </div>
     </div>
   </section>
@@ -36,13 +45,18 @@ export default {
   },
   data() {
     return {
-      gameCounter: 0
+      gameCounter: 0,
+      exists: true
     }
   },
   methods: {
     gameMinus() {
       if (this.games) {
         this.gameCounter--;
+        this.exists = !this.exists;
+        setTimeout(() => {
+          this.exists = !this.exists;
+        }, 300);
         if (this.gameCounter < 0) {
           this.gameCounter = this.games.length - 1;
         }
@@ -51,6 +65,10 @@ export default {
     gamePlus() {
       if (this.games) {
         this.gameCounter++;
+        this.exists = !this.exists;
+        setTimeout(() => {
+          this.exists = !this.exists;
+        }, 300);
         if (this.gameCounter > this.games.length - 1) {
           this.gameCounter = 0;
         }
@@ -61,6 +79,23 @@ export default {
 </script>
 
 <style scoped>
+.games-slide-enter-from {
+  opacity: 0;
+  scale: .9;
+}
+.games-slide-enter-to {
+  opacity: 1;
+  scale: 1;
+}
+.games-slide-leave-from {
+  opacity: 1;
+  scale: 1;
+}
+.games-slide-leave-to {
+  opacity: 0;
+  scale: .9;
+}
+
 .webpage-index { 
   width:          25rem;
   border-radius:  10px;
@@ -77,6 +112,7 @@ export default {
 }
 
 .arrow { 
+  z-index: 100;
   height:       2.5rem;
   position:     absolute;
   top:          200px;
@@ -111,6 +147,7 @@ export default {
 .actual-jsproject {
   display:          flex;
   flex-direction:   column;
+  transition:       all .2s ease-out;
 }
 
 .actual-jsproject a { text-align: center; }
@@ -122,6 +159,8 @@ export default {
   margin:       2.5rem 0;
   color:        var(--font-dark-color);
 }
+
+.project-link { margin: 0 40px; }
 
 .project-description { line-height: 23px; }
 </style>
--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -2,16 +2,30 @@
   <section class="tech-section" id="tech">
     <div class="container">
       <h2 class="section-title">Technológiák amiket jelenleg használok</h2>
+      
       <div class="row technology">
         <img class="technology-img" src="/computer-graphic.png" alt="technológiák index">
-        <div class="technologies-wrapper">
-          <div class="prog-wrapper" v-if="technologiesList.length > 0" v-for="tech in technologiesList" :key="tech.name">
-            <img class="technology-index" :src="tech.index" alt="vuejs logo">
+        
+        <TransitionGroup
+          name="tech"
+          tag="div"
+          class="technologies-wrapper">
+
+          <div class="prog-wrapper"
+            v-if="technologiesList.length > 0 && exists"
+            v-for="(tech, index) in technologiesList"
+            :key="tech.name"
+            :style="{ transitionDelay: `${index * .3}s` }">
+            <img class="technology-index"
+              :src="tech.index"
+              alt="vuejs logo">
             <p class="technology-name">{{ tech.name }}</p>
           </div>
-          <div class="error-msg" v-else><p>Nincsenek technlológiák!</p></div>
-        </div>
+        </TransitionGroup>
+        <!--<div class="error-msg" v-else><p>Nincsenek technlológiák!</p></div>-->
+        
       </div>
+
     </div>
   </section>
 </template>
@@ -25,13 +39,50 @@ export default {
         {name: "css", index: "/technology/css3-alt.svg"},
         {name: "js", index: "/technology/js.svg"},
         {name: "vue.js", index: "/technology/vue-9-logo.svg"}
-      ]
+      ],
+      exists: false
+    }
+  },
+
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+
+  methods: {
+    handleScroll() {
+      const element = this.$el.querySelector('.technologies-wrapper');
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+        if (rect.top <= windowHeight && rect.bottom >= 0) {
+          this.exists = true;
+        }
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.tech-enter-active {
+  transition: all 0.5s ease;
+}
+
+.tech-enter-from {
+  opacity: 0;
+  transform: translateY(50px);
+}
+
+.tech-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
 .tech-section {
   background-image: linear-gradient(#171923, #363c57, #171923);
 }
@@ -61,7 +112,7 @@ export default {
   padding:            1.25rem 3.125rem;
   border-radius:      15px 50px;
   background-color:   var(--section-light-bg);
-  color:              var(--font-dark-color)
+  color:              var(--font-dark-color);
 }
 
 .technology-name { padding-top: 0.625rem; }
